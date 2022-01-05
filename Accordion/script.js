c2b5c41128fa0a39661d869c4d35f8024d174896
Arpit Jana <arpitarannya@gmail.com>
2022-01-05 08:28:30 +0530

    ui modified

--- Accordion/script.js
+++ Accordion/script.js
@@ -5,22 +5,14 @@ let title = document.getElementsByClassName("title");
 let icon = document.getElementsByClassName("icon");
 let content = document.getElementsByClassName("content");
 
-// console.log(item, btnToggle, number, title, icon, content);
 for (let i = 0; i < btnToggle.length; i++) {
   btnToggle[i].addEventListener("click", () => {
-    console.log("Clicked");
-    console.log(parseInt(content[i].style.height.slice(0, -2)));
-    console.log(content[i].scrollHeight);
-    if (
-      Math.trunc(content[i].style.height.slice(0, -2)) !==
-      content[i].scrollHeight
-    ) {
+    if (parseInt(content[i].style.height) != content[i].scrollHeight) {
       content[i].style.height = content[i].scrollHeight + "px";
       icon[i].style.transform = "rotate(180deg)";
       number[i].style.color = "#6741d9";
       title[i].style.color = "#6741d9";
       icon[i].style.color = "#6741d9";
-      //   item[i].style.borderTop = "solid 5px #6741d9";
       btnToggle[i].style.backgroundColor = "#f3f0ff";
     } else {
       content[i].style.height = "0px";
@@ -28,18 +20,19 @@ for (let i = 0; i < btnToggle.length; i++) {
       number[i].style.color = "#adb5bd";
       title[i].style.color = "#343a40";
       icon[i].style.color = "#343a40";
-      //   item[i].style.borderTop = "solid 5px transparent";
       btnToggle[i].style.backgroundColor = "white";
     }
 
-    for (let j = 0; j < btnToggle.length; j++) {
+    console.log(parseInt(content[i].style.height));
+    console.log(content[i].scrollHeight);
+
+    for (let j = 0; j < content.length; j++) {
       if (j !== i) {
         content[j].style.height = "0px";
         icon[j].style.transform = "rotate(360deg)";
         number[j].style.color = "#adb5bd";
         title[j].style.color = "#343a40";
         icon[j].style.color = "#343a40";
-        // item[j].style.borderTop = "solid 5px transparent";
         btnToggle[j].style.backgroundColor = "white";
       }
     }
